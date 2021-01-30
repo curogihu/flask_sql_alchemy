4e3eb5323bac7e1d796a079e2a799b4f45e8f346
curogihu <curoasg@gmail.com>
2021-01-30 16:20:34 +0900

    the itemlist and create items

--- section_04/code/app.py
+++ section_04/code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
@@ -19,14 +19,24 @@ class Item(Resource):
 
     
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        data = request.get_json()
+
+        item = {'name': data['name'], 'price': data['price']}
         items.append(item)
 
         return item, 201
 
 
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
+
+
 
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
 
